1e7f963cb37141c65ce2539c8e25ac6966be1ec0
neerajpandit-innobles <neeraj.pandey@innobles.com>
2024-07-15 12:32:26 +0530

    work img

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(morganMiddleware);
 app.use(logRequestResponse);
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
+app.use("/public",express.static("public"));
 app.use(cookieParser());
 app.set("view engine", "ejs");
 app.use(
